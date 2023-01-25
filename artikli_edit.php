0c420e051000b61b0487281cee0a4ba54301703f
Svjetlana <svjetlanatodoric03@gmail.com>
2023-01-25 18:45:41 +0100

    Not done yet

---
+++ artikli_edit.php
@@ -0,0 +1,22 @@
+<head>
+    <link rel="stylesheet" href="style.css">
+    <style>
+        html {
+            background: unset;
+        }
+
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size: 18px;
+        }
+       
+    </style>
+</head>
+
+<div>
+    <p>
+        <a id="edit-a" href="artikli_list.php">Vratite se na pregled artikala</a>
+    </p>
+</div>
